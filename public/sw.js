490c39b90f32060ab64b95dfb3628ab219819306
lokeshKumar <lokeshkumargithub@gmail.com>
2021-07-03 12:04:32 +0530

    only build complite

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,49 @@
-console.log("sw from public folder")
+let catchData = "appV1";
+
+
+self.addEventListener("install",(event) =>{
+    event.waitUntil(
+        caches.open(catchData).then((cache)=>{
+            cache.addAll([
+
+                "static/js/bundle.js",
+                "static/js/main.chunk.js",
+                "static/js/0.chunk.js",
+                "/index.html",
+                "/static/js/vendors~main.chunk.js",
+                "/favicon.ico",
+                "/",
+                "/users",
+                "/manifest.json",
+                "/logo192.png",
+                "/testData",
+                "/about"
+
+
+            ])
+        })
+
+    )
+})
+
+self.addEventListener("fetch",(event) =>{
+    if(!navigator.onLine){
+        event.respondWith(
+            caches.match(event.request).then((res)=>{
+                if(res){
+                    return res
+                }
+                //api caches
+
+                let requestUrl= event.request.clone();
+
+                return fetch(requestUrl)
+    
+            })
+    
+        )
+
+    }
+
+})
+
--- src/User.js
+++ src/User.js
@@ -1,11 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import { Table } from 'react-bootstrap'
 
-export default class User extends Component {
-    render() {
-        return (
+export default function User() {
+
+    const [data, setData] = useState([])
+
+    const [mode,setMode] = useState('online')
+
+
+    useEffect(() => {
+        let Url = "https://jsonplaceholder.typicode.com/posts";
+        fetch(Url).then((res) => {
+            res.json().then((result) => {
+                //console.log("result", result)
+                setData(result)
+                localStorage.setItem("users",JSON.stringify(result))
+            })
+        }).catch(err => {
+            // alert("catch block")
+            let collection= localStorage.getItem("users")
+            setData(JSON.parse(collection))
+
+            setMode('offline')
+
+        })
+
+    }, [])
+
+
+    return (
+
+        <div>
+            <h1>user</h1>
+
             <div>
-                <h1>User</h1>
+                {
+                    mode === 'offline'?
+                    <div> yoy are offline</div>:
+                    <div>online</div>
+                }
             </div>
-        )
-    }
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>TITLE</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item,i) => (
+                            <tr>
+                                <td>{i+1}</td>
+                                <td>{item.title}</td>
+
+                            </tr>
+
+                        ))
+                    }
+
+                </tbody>
+            </Table>
+        </div>
+    );
 }
+
+
+
+
+
